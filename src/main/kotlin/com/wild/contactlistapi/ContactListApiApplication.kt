fc614356f544d4f3548ffc6446cea52a19abdeb7
TomasWild <tomaswild@outlook.com>
2023-12-09 15:07:06 -0300

    Initial commit

---
+++ src/main/kotlin/com/wild/contactlistapi/ContactListApiApplication.kt
@@ -0,0 +1,11 @@
+package com.wild.contactlistapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContactListApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ContactListApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/wild/contactlistapi/ContactListApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wild.contactlistapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ContactListApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
